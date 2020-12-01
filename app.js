edd38d8c81417adb47cfa03f8c0c29fc9a9b34bd
Ramon Andrade <andrade.rmn@gmail.com>
2020-12-01 02:32:06 -0300

    deploy

--- app.js
+++ app.js
@@ -302,8 +302,7 @@ app.step(new WorkflowStep('techops.request.workflow.finished', {
       let requested_at = techopsStatuses.find(status => status['status'] == 'OPEN')['created_at'];
       let assigned_at = techopsStatuses.find(status => status['status'] == 'SOLVING')['created_at'];
       let finished_at = techopsStatuses.find(status => status['status'] == 'FINISHED')['created_at'];
-      // var diff = Math.abs(new Date(finished_at) - new Date(requested_at));
-      // let minutes = Math.floor((diff/1000)/60);
+      
       let minutes = Math.round(moment(finished_at).diff(moment(requested_at), 'minutes'))
 
       // 2020-12-01T04:58:39.061Z

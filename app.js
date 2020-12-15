7a260444f5fd41e285cc714d244f54ea93baf8ec
Ramon Andrade <andrade.rmn@gmail.com>
2020-12-15 10:33:01 -0300

    deploy

--- app.js
+++ app.js
@@ -572,6 +572,9 @@ app.action('send_message', async ({ ack, body, context, client }) => {
 
   console.log('==============================');
   console.log(payload);
+  console.log('==============================');
+  console.log(body);
+  console.log('==============================');
   await messages.recurrenceWhatsappSent(app, context.botToken, payload)
   
   // console.log(body['container']);
